655b7884a8fc7b9ca75c5bb995f5e09854228fbd
Zack Ye <60680461+Cirrhosis233@users.noreply.github.com>
2024-12-06 17:45:00 -0500

    deepseek sqlcoder v2

--- finetune.sh
+++ finetune.sh
@@ -8,10 +8,12 @@
 
 # python finetune_v1.py ./configs/codellama-v7.4.json 
 
-python finetune_v1.py ./configs/codellama-v8.json 
-python finetune_v1.py ./configs/codellama-v8.1.json 
-python finetune_v1.py ./configs/codellama-v8.2.json 
+# python finetune_v1.py ./configs/codellama-v8.json 
+# python finetune_v1.py ./configs/codellama-v8.1.json 
+# python finetune_v1.py ./configs/codellama-v8.2.json 
 
-python finetune_v1.py ./configs/deepseek-v1.json 
+# python finetune_v1.py ./configs/deepseek-v1.json 
+python finetune_v1.py ./configs/deepseek-v2.json 
 
-python finetune_v1.py ./configs/sqlcoder-v1.json 
+# python finetune_v1.py ./configs/sqlcoder-v1.json 
+python finetune_v1.py ./configs/sqlcoder-v2.json 
--- inference_vllm.sh
+++ inference_vllm.sh
@@ -16,30 +16,30 @@
 # python inference_vllm.py --model deepseek --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/deepseek_v0
 # python inference_vllm.py --model deepseek --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/deepseek_v0
 
-python inference_vllm.py --model codellama --data val --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8 --lora models/codellama-v8
-python inference_vllm.py --model codellama --data val --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8.1 --lora models/codellama-v8.1
-python inference_vllm.py --model codellama --data val --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8.2 --lora models/codellama-v8.2
+# python inference_vllm.py --model codellama --data val --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8 --lora models/codellama-v8
+# python inference_vllm.py --model codellama --data val --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8.1 --lora models/codellama-v8.1
+# python inference_vllm.py --model codellama --data val --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8.2 --lora models/codellama-v8.2
 
-python inference_vllm.py --model codellama --data test --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8 --lora models/codellama-v8
-python inference_vllm.py --model codellama --data test --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8.1 --lora models/codellama-v8.1
-python inference_vllm.py --model codellama --data test --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8.2 --lora models/codellama-v8.2
+# python inference_vllm.py --model codellama --data test --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8 --lora models/codellama-v8
+# python inference_vllm.py --model codellama --data test --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8.1 --lora models/codellama-v8.1
+# python inference_vllm.py --model codellama --data test --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/codellama_v8.2 --lora models/codellama-v8.2
 
-python inference_vllm.py --model codellama --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8 --lora models/codellama-v8
-python inference_vllm.py --model codellama --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8.1 --lora models/codellama-v8.1
-python inference_vllm.py --model codellama --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8.2 --lora models/codellama-v8.2
+# python inference_vllm.py --model codellama --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8 --lora models/codellama-v8
+# python inference_vllm.py --model codellama --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8.1 --lora models/codellama-v8.1
+# python inference_vllm.py --model codellama --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8.2 --lora models/codellama-v8.2
 
-python inference_vllm.py --model codellama --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8 --lora models/codellama-v8
-python inference_vllm.py --model codellama --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8.1 --lora models/codellama-v8.1
-python inference_vllm.py --model codellama --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8.2 --lora models/codellama-v8.2
+# python inference_vllm.py --model codellama --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8 --lora models/codellama-v8
+# python inference_vllm.py --model codellama --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8.1 --lora models/codellama-v8.1
+# python inference_vllm.py --model codellama --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/codellama_v8.2 --lora models/codellama-v8.2
 
-python inference_vllm.py --model deepseek --data val --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/deepseek_v1 --lora models/deepseek-v1
-python inference_vllm.py --model deepseek --data test --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/deepseek_v1 --lora models/deepseek-v1
+python inference_vllm.py --model deepseek --data val --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/deepseek_v2 --lora models/deepseek-v2
+python inference_vllm.py --model deepseek --data test --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/deepseek_v2 --lora models/deepseek-v2
 
-python inference_vllm.py --model deepseek --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/deepseek_v1 --lora models/deepseek-v1
-python inference_vllm.py --model deepseek --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/deepseek_v1 --lora models/deepseek-v1
+python inference_vllm.py --model deepseek --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/deepseek_v2 --lora models/deepseek-v2
+python inference_vllm.py --model deepseek --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/deepseek_v2 --lora models/deepseek-v2
 
-python inference_vllm.py --model sqlcoder --data val --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/sqlcoder_v1 --lora models/sqlcoder-v1
-python inference_vllm.py --model sqlcoder --data test --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/sqlcoder_v1 --lora models/sqlcoder-v1
+python inference_vllm.py --model sqlcoder --data val --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/sqlcoder_v2 --lora models/sqlcoder-v2
+python inference_vllm.py --model sqlcoder --data test --prompt prompts/prompt_v4.md --output inference_output/prompt_v4/sqlcoder_v2 --lora models/sqlcoder-v2
 
-python inference_vllm.py --model sqlcoder --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/sqlcoder_v1 --lora models/sqlcoder-v1
-python inference_vllm.py --model sqlcoder --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/sqlcoder_v1 --lora models/sqlcoder-v1
+python inference_vllm.py --model sqlcoder --data eval_train --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/sqlcoder_v2 --lora models/sqlcoder-v2
+python inference_vllm.py --model sqlcoder --data eval_test --prompt prompts/prompt_v4_postgres.md --output inference_output/prompt_v4/sqlcoder_v2 --lora models/sqlcoder-v2
